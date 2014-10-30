846e04e003e72e4e64c3b3d1e9a63d0333ecae15
menertry <menertry@outlook.com>
2014-10-30 19:38:03 +0800

    nothing

--- crawle_page.py
+++ crawle_page.py
@@ -3,25 +3,45 @@ import urllib2
 import re
 
 def get_url(html)
-    reg = r'regex'
+    reg = r'previewThread[\s\S]+?onclick'
     imgre = re.compile(reg)
     imglist = re.findall(imgre, html)
     counter = 0
     for url in imglist 
-        imglist[counter] = url[]
+        string = url
+        index = string.find("viewthread")
+        string = string[index:-9]
+        imglist[counter] = string
+        counter += 1
     return imglist
 
 def if_end(html)
-    reg = r'regex'
+    if current_page(html) == total_page(html)
+        return 1
+    else
+        return 0
+
+def current_page(html)
+    reg = r'curpage="[\s\S]+?"'
+    imgre = re.compile(reg)
+    imglist = re.findall(reg, html)
+    current_page_str = imglist[0][9:-1]
+    current_page = string.atoi(current_page_str)#!!!!!
+    return current_page
+
+def total_page(html)
+    reg = r'totalpage="[\s\S]+?"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre, html)
-
-    return 1
-
+    total_page_str = imglist[0][11:-1]
+    total_page = string.atoi(total_page_str)
+    return total_page
+    
 def get_page(url, curosr)
     html = urllib2.urlopen(url).read()
     html_utf8 = html.decode('gbk').encode('utf8')
     url_list = get_url(html_utf8)
     for url in url_list
         get_post(url, cursor)
-    return if_end(html_utf8)
+    return
+        if_end(html_utf8)
--- crawler.py
+++ crawler.py
@@ -2,22 +2,20 @@ import urllib
 import urllib2
 import re
 import MySQLdb
-
-#html = urllib2.urlopen('http://bbs.hackbase.com/forum.php?mod=viewthread&tid=3598460&extra=page%3D1').read();
-#get html page
-
-#reg = r'<table cellspacing="0" cellpadding="0"><tr><td class="t_f" id="postmessage_.+?</table>'
-
+import login
+import crawle_page
 
 db = MySQLdb.connect("localhost","root","","mysql")
 cursor = db.cursor()
+
+login.login()
 
 url = ""
 page_url = url
 html = urllib2.urlopen(page_url)
 page = 1
 
-while get_page(page_url) != 1
+while crawle_page.get_page(page_url, cursor) != 1
     page += 1
     page_url = url + "&page=" + string.itoa(page)
 
