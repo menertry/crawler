b0c163062686fb6f8646ee6ceac6b489312c45cb
menertry <menertry@outlook.com>
2014-10-30 16:49:29 +0800

    login

---
+++ login.py
@@ -0,0 +1,38 @@
+#! /usr/python
+import urllib
+import urllib2
+import re
+import cookielib
+
+def get_loginhash(html):
+    reg = r'loginhash[\s\S]+?"'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    loginhash = imglist[0][10:-1]
+    return loginhash
+
+def get_formhash(html):
+    reg = r'formhash[\s\S]+?/'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    formhash = imglist[0][17:-3]
+    return formhash
+
+def get_seccode(html):
+    reg = r'seccode[\s\S]+?"'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    seccode = imglist[8:-1]
+    return seccode
+
+def login():
+    loginurl = 'http://bbs.hackbase.com/member.php?mod=logging&action=login'
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+    html = urllib2.urlopen(loginurl).read()
+    posturl = 'http://bbs.hackbase.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash='+get_loginhash(html)+'&inajax=1'
+    data ={'formhash':get_formhash(html),'referer':'http://bbs.hackbase.com/forum.php','username':'menertry','password':'8e4ca7af923595919610e17f0dde5140','questionid':'0','answer':'','seccodehash':get_seccode(html),'seccodemodid':'member::logging','seccodeverify':''}
+    post_data = urllib.urlencode(data)
+    req = urllib2.Request(posturl,post_data)
+    urllib2.urlopen(req)
